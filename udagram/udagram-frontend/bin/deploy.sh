44d8b5880f33b90a91eea3a8880f73182914efa2
Abdulaziz Omar <elabdulazizomar@gmail.com>
2022-07-03 00:05:20 +0200

    fix: circle ci config hold issue

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3 cp --recursive --acl public-read www/ s3://udagram-full-stack74/
+# aws s3 sync cp --recursive --acl public-read ./www s3://udagram-full-stack74/
+# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-full-stack74/
